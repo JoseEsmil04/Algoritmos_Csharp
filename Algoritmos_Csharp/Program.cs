db009a41b091269757eda8921c04b2c3c2836c7f
JoseEsmil04 <josecampusano0427@gmail.com>
2024-09-28 21:25:47 -0400

    Agregar archivos de proyecto.

---
+++ Algoritmos_Csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+using Algoritmos_Csharp.Lvl1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string cadena = "holaesmilaaaaaesmil";
+        string sub = "esmil";
+
+        var result = Subcadena.BuscarSub(cadena, sub);
+        
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+        
+    }
+}
